26324382a03b74c5bc187a26144c7eee64c1a7d3
Wes Modes <wmodes@gmail.com>
2023-10-20 12:13:53 -0700

    totaal reorg

---
+++ art101_conf.py
@@ -0,0 +1,211 @@
+# ART101 Configuration
+#
+
+OBJECTIVES = [
+    {
+        "field": "schedule 1",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 2",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 3",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 4",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 5",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 6",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 7",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 8",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 9",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 10",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 11",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 12",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 13",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 14",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 15",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "schedule 16",
+        "measure": "similar",
+        "weight": 1/16,
+    },
+    {
+        "field": "expertise 1",
+        "measure": "diverse",
+        "weight": 1/4,
+    },
+    {
+        "field": "expertise 2",
+        "measure": "diverse",
+        "weight": 1/4,
+    },
+    {
+        "field": "expertise 3",
+        "measure": "diverse",
+        "weight": 1/4,
+    },
+    {
+        "field": "expertise 4",
+        "measure": "diverse",
+        "weight": 1/4,
+    },
+    {
+        "field": "committment",
+        "measure": "similar",
+        "weight": 1/5 * 3,
+    },
+    {
+        "field": "leadership",
+        "measure": "diverse",
+        "weight": 1/5 * 2,
+    },
+    {
+        "field": "group organization",
+        "measure": "similar",
+        "weight": 1/5 * 2,
+    },
+    {
+        "field": "big picture",
+        "measure": "diverse",
+        "weight": 1/5 * 2,
+    },
+    {
+        "field": "social",
+        "measure": "similar",
+        "weight": 1/5 * 2,
+    },
+    {
+        "field": "interest 1",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 2",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 3",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 4",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 5",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 6",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 7",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 8",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 9",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 10",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 11",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 12",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 13",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 14",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+    {
+        "field": "interest 15",
+        "measure": "similar",
+        "weight": 1/15,
+    },
+]
+
+PREFERENCES = {
+    # looking at the gatorgrouper code, I don't think these get passed 
+    "weight": -1.1,
+    "matchweight": -1.3
+}
